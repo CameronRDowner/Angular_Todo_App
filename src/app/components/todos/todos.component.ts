3d7ab3db938c0805727a1e7267c69efc9f47e400
Cameron Downer <cam198@gmail.com>
2019-12-22 19:58:30 -0600

    todo-items fade in when created and fade out when deleted

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,25 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { TodoService } from '../../services/todo.service';
 import {Todo} from '../../models/Todo'
-import { trigger, transition, style, animate, state } from '@angular/animations';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.scss'],
   animations: [
-    trigger('fade', [      
+    trigger('fade', [
       transition('void => *', [
-        style({opacity: 0}),
-        animate(1000, style({opacity: 1}))
+        style({ opacity: 0 }),
+        animate(500, style({ opacity: 1 }))
       ]),
       transition('* => void', [
-        animate(1000, style({opacity: 0}))
+        animate(500, style({ opacity: 0 }))
       ])
-
     ])
-    
-]
+  ]
 })
 export class TodosComponent implements OnInit {
   todos:Todo[];
@@ -30,7 +28,7 @@ export class TodosComponent implements OnInit {
     this.todoService.getTodos().subscribe(todos=> { this.todos = todos});
   }
   deleteTodo(todo:Todo){
-    this.todos.splice(this.todos.indexOf(todo), 1);
+      this.todos.splice(this.todos.indexOf(todo), 1);
     this.todoService.deleteTodo(todo).subscribe();
   }
   addTodo(todo:Todo){
