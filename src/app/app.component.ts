d2a137a2406e296d32c0f1850cb55fbf178173cc
Cameron Downer <cam198@gmail.com>
2019-12-26 17:51:25 -0600

    added animations to todo-item, made delete todo buttons square, is-completed class now applies to just the todo-item text, completed todo-items have their checkboxes automatically checked

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { style, state, trigger, transition, animate} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('slideInDown', [
+      state('void', style({transform: 'translateY(-1000px)', opacity: 0})),
+      state('inDom', style({transform: 'translateY(0px)', opacity: 1})),
+      transition('void => *', [
+        animate('500ms ease-in')
+      ])
+
+    ])
+  ]
 })
 export class AppComponent {
   name:string = 'Cameron';
